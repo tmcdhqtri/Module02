459dead4c15d2c1ff343faabcfcc421d829d5638
Tran Manh Cuong <cunbeo04012017@gmail.com>
2022-10-31 16:00:49 +0700

    Redux Actions

--- src/components/DishdetailComponent .js
+++ src/components/DishdetailComponent .js
@@ -25,8 +25,7 @@ class CommentForm extends Component {
    }
    
    handleSubmit(values) {
-      console.log("Current State is: " + JSON.stringify(values));
-      alert("Current State is: " + JSON.stringify(values));
+      this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
       this.toggleModal();
    }
    
@@ -110,7 +109,7 @@ function RenderDish({dish}) {
    );
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, dishId}) {
    if (comments != null) {
       return (
          <div className="col-12 col-md-5 m-1">
@@ -125,7 +124,7 @@ function RenderComments({comments}) {
                );
             })}
             </ul>
-            <CommentForm />
+            <CommentForm dishId={dishId} addComment={addComment} />
          </div>
       );
    }
@@ -152,7 +151,10 @@ const Dishdetail = (props) => {
             </div>
             <div className="row">
                <RenderDish dish={props.dish} />
-               <RenderComments comments={props.comments} />
+               <RenderComments comments={props.comments}
+        addComment={props.addComment}
+        dishId={props.dish.id}
+      />
             </div>
          </div>
       );
