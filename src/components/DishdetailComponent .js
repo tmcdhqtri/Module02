98faf6905c60759dc58357209f56c9d91d929599
Tran Manh Cuong <cunbeo04012017@gmail.com>
2022-10-31 16:25:17 +0700

    Redux Thunk

--- src/components/DishdetailComponent .js
+++ src/components/DishdetailComponent .js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Modal, ModalHeader, ModalBody, Button, Label, Col, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -136,7 +137,25 @@ function RenderComments({comments, addComment, dishId}) {
 }
 
 const Dishdetail = (props) => {
-   if (props.dish != null) {
+   if (props.isLoading) {
+      return(
+          <div className="container">
+              <div className="row">            
+                  <Loading />
+              </div>
+          </div>
+      );
+  }
+  else if (props.errMess) {
+      return(
+          <div className="container">
+              <div className="row">            
+                  <h4>{props.errMess}</h4>
+              </div>
+          </div>
+      );
+  }
+  else if (props.dish != null) {
       return (
          <div className="container">
             <div className="row">
