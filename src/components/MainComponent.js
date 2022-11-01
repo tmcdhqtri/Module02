d65130947aa4307be5f5547f81b7684c152e4dce
Tran Manh Cuong <cunbeo04012017@gmail.com>
2022-11-02 00:27:07 +0700

    Fetch Post Comment

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,12 +9,7 @@ import Footer from "./FooterComponent";
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { actions } from "react-redux-form";
-import {
-  addComment,
-  fetchDishes,
-  fetchComments,
-  fetchPromos,
-} from "../redux/ActionCreators";
+import { postComment, fetchDishes, fetchComments, fetchPromos } from "../redux/ActionCreators";
 
 const mapStateToProps = (state) => {
   return {
@@ -26,8 +21,7 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = (dispatch) => ({
-  addComment: (dishId, rating, author, comment) =>
-    dispatch(addComment(dishId, rating, author, comment)),
+  postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment)),
   fetchDishes: () => {
     dispatch(fetchDishes());
   },
@@ -82,7 +76,7 @@ class Main extends Component {
             (comment) => comment.dishId === parseInt(match.params.dishId, 10)
           )}
           commentsErrMess={this.props.comments.errMess}
-          addComment={this.props.addComment}
+          postComment={this.props.postComment}
         />
       );
     };
